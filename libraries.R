cc958f046cb1cefea0ad290d81d3702478a2c504
Jon <jonathan.rummel@venturatravel.com>
2019-07-28 10:35:33 +0200

    first commit of essential packages & functions

---
+++ libraries.R
@@ -0,0 +1,28 @@
+#########################################################
+# This script contains basic packages that can be used 
+# for visualization, ETL and statistical analysis. 
+#
+# By: Jonathan Rummel
+#########################################################
+
+### load data manipulation packages
+pkgTest('dplyr')
+pkgTest('tidyverse')
+pkgTest('reshape2')
+### load text & reading packages
+pkgTest('stringr')
+### load visualization 
+pkgTest('ggplot2')
+pkgTest('plotly')
+pkgTest('maps')
+### load web querying/scraping packages 
+pkgTest('rvest')
+pkgTest('httr')
+pkgTest('RCurl')
+pkgTest('streamR')
+### load statistical packages
+pkgTest('stargazer')
+pkgTest('nnet')
+pkgTest('effects')
+pkgTest('foreign')
+pkgTest('plm')
---
+++ pkgTest.R
@@ -0,0 +1,9 @@
+# Function for testing whether a package exists, if yes, then load, if not, then install.
+pkgTest <- function(x)
+{
+  if (!require(x,character.only = TRUE))
+  {
+    install.packages(x,dep=TRUE)
+    if(!require(x,character.only = TRUE)) stop("Package not found")
+  }
+}
